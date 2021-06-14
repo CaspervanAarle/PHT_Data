dff0eefe5e3e7e91c252bbdb9461f6b08913ee72
CaspervanAarle <cas.v.aarle@gmail.com>
2021-06-14 12:24:53 +0200

    code separated in functions

--- converter.py
+++ converter.py
@@ -6,11 +6,7 @@ Created on Mon May 17 11:31:11 2021
 """
 
 import os
-import csv
-import shutil
-from random import shuffle
 import random
-import numpy as np
 import pandas as pd
 
 
@@ -19,48 +15,62 @@ import pandas as pd
 # must include header as first row
 dataset_name = "placeholder"
 
-### in
+### in dir:
 CSV_DIR = os.getcwd() + "\\in\\{}.csv".format(dataset_name)
-### out
+### out dir:
 GOAL_DIR_CENTRAL = os.getcwd() + "\\out\\{}\\".format(dataset_name + "_central")
 GOAL_DIR_FEDERATED = os.getcwd() + "\\out\\{}\\".format(dataset_name + "_federated")
 
-try:
-    OUT_DIR = os.getcwd() + "\\out\\"
-    os.mkdir(OUT_DIR)
-except OSError:
-    print ("Creation of the directory failed or already exists")   
+### random seed for consistent shuffling
+RANDOM_SEED = 4
 
+def write_federated(ds):
+    """make directory for federated data and write data"""
+    try:
+        os.mkdir(GOAL_DIR_FEDERATED)
+    except OSError:
+        print ("Creation of the directory failed or already exists")   
+        
+    ### write federated:
+    r_list = list(range(len(ds.index)))
+    random.Random(RANDOM_SEED).shuffle(r_list)
+    for i in range(min(len(r_list),1000)):
+        locker_df = ds.loc[[r_list[i]]]
+        locker_df.to_csv(GOAL_DIR_FEDERATED + "{}.csv".format(i+1),  index=False)
 
-### write central
-try:
-    os.mkdir(GOAL_DIR_CENTRAL)
-except OSError:
-    print ("Creation of the directory failed or already exists")   
-target = GOAL_DIR_CENTRAL + "data.csv"
-
-df = pd.read_csv(CSV_DIR, header=0)
-
-### shuffle
-ds = df.sample(frac=1)
-print(ds)
-
-
-### write federated
-try:
-    os.mkdir(GOAL_DIR_FEDERATED)
-except OSError:
-    print ("Creation of the directory failed or already exists")   
+def write_central(ds):
+    """make directory for central data and write data"""
+    ### make central directory:
+    try:
+        os.mkdir(GOAL_DIR_CENTRAL)
+    except OSError:
+        print ("Creation of the directory failed or already exists")   
+    target = GOAL_DIR_CENTRAL + "data.csv"
     
-r_list = list(range(len(ds.index)))
-random.Random(4).shuffle(r_list)
-#print(r_list)
-for i in range(min(len(r_list),1000)):
-    locker_df = ds.loc[[r_list[i]]]
-    #print(locker_df)
-    locker_df.to_csv(GOAL_DIR_FEDERATED + "{}.csv".format(i+1),  index=False)
-
-
-### write central    
-total_df = ds.loc[r_list]
-total_df.to_csv(target,  index=False)
+    ### write central:
+    r_list = list(range(len(ds.index)))
+    random.Random(RANDOM_SEED).shuffle(r_list)
+    total_df = ds.loc[r_list]
+    total_df.to_csv(target,  index=False)
+    
+    
+if __name__ == "__main__" :
+    ### read 
+    df = pd.read_csv(CSV_DIR, header=0)
+    
+    ### shuffle
+    ds = df.sample(frac=1)
+    print(ds)
+    
+    ### make out directory:
+    try:
+        OUT_DIR = os.getcwd() + "\\out\\"
+        os.mkdir(OUT_DIR)
+    except OSError:
+        print ("Creation of the directory failed or already exists")   
+        
+    ### fill with data:
+    write_central(ds)
+    write_federated(ds)
+    
+    
--- generator.py
+++ generator.py
@@ -13,83 +13,114 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
+# name your custom dataset:
+dataset_name = "synthetic"
 
-DATASET_NAME = "synthetic"
-GOAL_DIR_CENTRAL = os.getcwd() + "\\out\\{}\\".format(DATASET_NAME + "_central")
-GOAL_DIR_FEDERATED = os.getcwd() + "\\out\\{}\\".format(DATASET_NAME + "_federated")
+
+GOAL_DIR_CENTRAL = os.getcwd() + "\\out\\{}\\".format(dataset_name + "_central")
+GOAL_DIR_FEDERATED = os.getcwd() + "\\out\\{}\\".format(dataset_name + "_federated")
 N_FEATURES = 1
 N_TARGETS = 1
 
-OUT_DIR = os.getcwd() + "\\out\\"
-try:
-    os.mkdir(OUT_DIR)
-except OSError:
-    print ("Creation of the directory failed or already exists")   
-# create variable values
+
+def make_dir():
+    """generate out directory"""
+    OUT_DIR = os.getcwd() + "\\out\\"
+    try:
+        os.mkdir(OUT_DIR)
+    except OSError:
+        print ("Creation of the directory failed or already exists")  
+    return
 
 
 
-x_all, y_all, coef = sklearn.datasets.make_regression(n_samples=100, 
-                                 n_features=N_FEATURES, 
-                                 n_informative=N_FEATURES, 
-                                 n_targets=N_TARGETS, 
-                                 bias=2, 
-                                 effective_rank=None, 
-                                 tail_strength=-1, 
-                                 noise=1, 
-                                 shuffle=True, 
-                                 coef=True, 
-                                 random_state=9)
 
 
-
-"""
-x_all, y_all = sklearn.datasets.make_classification(n_samples=50, 
+def make_reg():
+    """generate dataset for regression task"""
+    return sklearn.datasets.make_regression(n_samples=100, 
                                      n_features=N_FEATURES, 
-                                     n_informative=1, 
-                                     n_redundant=0, 
-                                     n_repeated=0, 
-                                     n_classes=2, 
-                                     n_clusters_per_class=1, 
-                                     weights=None, 
-                                     flip_y=0.01, 
-                                     class_sep=0.5, 
-                                     hypercube=True, 
-                                     shift=1.0, 
-                                     scale=4.0, 
+                                     n_informative=N_FEATURES, 
+                                     n_targets=N_TARGETS, 
+                                     bias=2, 
+                                     effective_rank=None, 
+                                     tail_strength=-1, 
+                                     noise=1, 
                                      shuffle=True, 
-                                     random_state=5)
+                                     coef=False, 
+                                     random_state=9)
 
-"""
-plt.scatter(x_all, y_all)
-plt.show()
-#print(coef)
-# create variable names
-variables = []
-for x in range(N_FEATURES + N_TARGETS):
-    variables.append("var_" + str(x+1))
+
+
+def make_classif():
+    """generate dataset for classification task"""
+    return sklearn.datasets.make_classification(n_samples=50, 
+                                         n_features=N_FEATURES, 
+                                         n_informative=1, 
+                                         n_redundant=0, 
+                                         n_repeated=0, 
+                                         n_classes=2, 
+                                         n_clusters_per_class=1, 
+                                         weights=None, 
+                                         flip_y=0.01, 
+                                         class_sep=0.5, 
+                                         hypercube=True, 
+                                         shift=1.0, 
+                                         scale=4.0, 
+                                         shuffle=True, 
+                                         random_state=5)
+
+def create_header():
+    """creates header for csv file"""
+    variables = []
+    for x in range(N_FEATURES + N_TARGETS):
+        variables.append("var_" + str(x+1))
+    return variables
+
+
+def visualize(x_all, y_all):
+    """visualize generated datapoints when n_features=1"""
+    if np.array(x_all).shape[1] == 1:
+        plt.scatter(x_all, y_all)
+        plt.show()
+    else:
+        print("Unable to visualize input variables containing more than 1 feature")
     
-#  save to single csv file:
-try:
-    os.mkdir(GOAL_DIR_CENTRAL)
-except OSError:
-    print ("Creation of the directory failed or already exists")    
-y_all_s = np.expand_dims(y_all, axis=1)
-all_data = np.concatenate((x_all,y_all_s), axis=1)
-with open(GOAL_DIR_CENTRAL + 'data.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(variables)
-    for d in all_data:
-        writer.writerow(d)
-
-# save to separate csv files:
-try:
-    os.mkdir(GOAL_DIR_FEDERATED)
-except OSError:
-    print ("Creation of the directory failed or already exists")
-for i, (x, y) in enumerate(zip(x_all, y_all)):
-    datapoint = np.concatenate((x, [y]))
-    with open(GOAL_DIR_FEDERATED + str(i+1) + '.csv', 'w', newline='') as file:
+        
+def write_central(x_all, y_all, header):
+    """save to single csv file:"""
+    try:
+        os.mkdir(GOAL_DIR_CENTRAL)
+    except OSError:
+        print ("Creation of the directory failed or already exists")    
+    y_all_s = np.expand_dims(y_all, axis=1)
+    all_data = np.concatenate((x_all,y_all_s), axis=1)
+    with open(GOAL_DIR_CENTRAL + 'data.csv', 'w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(variables)
-        writer.writerow(datapoint)
+        writer.writerow(header)
+        for d in all_data:
+            writer.writerow(d)
+        
+def write_federated(x_all, y_all, header):
+    """save to separate csv files"""
+    try:
+        os.mkdir(GOAL_DIR_FEDERATED)
+    except OSError:
+        print ("Creation of the directory failed or already exists")
+    for i, (x, y) in enumerate(zip(x_all, y_all)):
+        datapoint = np.concatenate((x, [y]))
+        with open(GOAL_DIR_FEDERATED + str(i+1) + '.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+            writer.writerow(datapoint)
+            
+if __name__ == "__main__" :
+    make_dir()
+    # x_all, y_all = make_reg()
+    x, y = make_classif()
+    visualize(x, y)
+    header = create_header()
+    write_central(x, y, header)
+    write_federated(x, y, header)
+    
+    
